fb40735c3097f447894e70ae5f1e680271461a02
innocentmpeta <innompeta@gmail.com>
2026-07-09 13:17:11 +0200

    Remove old Express server — now using Firebase

--- server/routes/site.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/site/index.html'));
-});
-
-module.exports = router;
